a42f594cfc5153246b0ddd4d97de03a96517a663
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-08-12 11:55:06 +0900

    MapDemoPage(仮)

--- lib/demo_pages/mapdemo_page.dart
+++ lib/demo_pages/mapdemo_page.dart
@@ -14,6 +14,8 @@ class MapDemoPage extends StatelessWidget {
   MapDemoPage({super.key});
 
   final TransformationController _controller = TransformationController();
+  final _controllerX = ScrollController();
+  final _controllerY = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,78 +30,99 @@ class MapDemoPage extends StatelessWidget {
             children: [
               Consumer(
                 builder: (context, ref, child) {
-                  final scale = ref.watch(_mapDemoProvider);
-                  _controller.value = Matrix4.identity()..scale(scale);
-                  return InteractiveViewer(
-                    transformationController: _controller,
-                    //boundaryMarginで拡大した時に端っこを中心まで持ってこれる。
-                    boundaryMargin: const EdgeInsets.all(20.0),
-                    minScale: 1.0,
-                    maxScale: 15.0,
-                    child: SingleChildScrollView(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Column(
-                          children: List.generate(
-                            100,
-                            (rowIndex) {
-                              return Row(
-                                children: List.generate(100, (colIndex) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(0.5),
-                                    child: Container(
-                                      height: 5,
-                                      width: 5,
-                                      color: Colors.green,
-                                    ),
+                  // final scale = ref.watch(_mapDemoProvider);
+                  // _controller.value = Matrix4.identity()..scale(scale);
+                  return Stack(
+                    children: [
+                      InteractiveViewer(
+                        //斜めに動かせるかどうか
+                        // constrained: false,
+                        transformationController: _controller,
+                        //boundaryMarginで拡大した時に端っこを中心まで持ってこれる。
+                        boundaryMargin: const EdgeInsets.all(20.0),
+                        minScale: 1.0,
+                        maxScale: 15.0,
+                        child: SingleChildScrollView(
+                          controller: _controllerY,
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            controller: _controllerX,
+                            child: Column(
+                              children: List.generate(
+                                100,
+                                (rowIndex) {
+                                  return Row(
+                                    children: List.generate(100, (colIndex) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(0.5),
+                                        child: Container(
+                                          height: 5,
+                                          width: 5,
+                                          color: Colors.green,
+                                        ),
+                                      );
+                                    }),
                                   );
-                                }),
-                              );
-                            },
+                                },
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                      GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        child: const SizedBox(
+                          width: double.infinity,
+                          height: double.infinity,
+                        ),
+                        onPanUpdate: (DragUpdateDetails data) {
+                          _controllerX.jumpTo(
+                              _controllerX.offset + (data.delta.dx * -1));
+                          _controllerY.jumpTo(
+                              _controllerY.offset + (data.delta.dy * -1));
+                        },
+                      ),
+                    ],
                   );
                 },
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 280.0,
-                      child: Consumer(
-                        builder: (context, ref, child) {
-                          return Slider(
-                            value: ref.watch(_mapDemoProvider),
-                            onChanged: (value) {
-                              ref
-                                  .read(_mapDemoProvider.notifier)
-                                  .changeState(value);
-                            },
-                            min: 1.0,
-                            max: 15.0,
-                          );
-                        },
-                      ),
-                    ),
-                    Consumer(
-                      builder: (context, ref, child) {
-                        return FloatingActionButton(
-                          onPressed: () => {
-                            print(
-                              ref.watch(_mapDemoProvider),
-                            ),
-                          },
-                          child: const Icon(Icons.add),
-                        );
-                      },
-                    )
-                  ],
-                ),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(top: 15.0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       SizedBox(
+              //         width: 280.0,
+              //         child: Consumer(
+              //           builder: (context, ref, child) {
+              //             return Slider(
+              //               value: ref.watch(_mapDemoProvider),
+              //               onChanged: (value) {
+              //                 ref
+              //                     .read(_mapDemoProvider.notifier)
+              //                     .changeState(value);
+              //               },
+              //               min: 1.0,
+              //               max: 15.0,
+              //             );
+              //           },
+              //         ),
+              //       ),
+              //       Consumer(
+              //         builder: (context, ref, child) {
+              //           return FloatingActionButton(
+              //             onPressed: () => {
+              //               print(
+              //                 ref.watch(_mapDemoProvider),
+              //               ),
+              //             },
+              //             child: const Icon(Icons.add),
+              //           );
+              //         },
+              //       )
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
